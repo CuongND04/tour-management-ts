c3696e6a0396715e117cdeed105ebcb783e1f63b
Nguyễn Đức Cường <uteu237@gmail.com>
2024-08-17 21:17:58 +0700

    ♻️ create controllers

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -1,22 +1,6 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 const router: Router = Router();
 
-import Tour from "../../models/tour.model";
-
-router.get("/", async (req: Request, res: Response) => {
-  // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active",
-    },
-    raw: true,
-  });
-
-  res.render("client/pages/tours/index", {
-    pageTitle: "Danh sách tour",
-    tours: tours,
-  });
-});
-
+import * as controller from "../../controllers/client/tour.controller";
+router.get("/", controller.index);
 export const tourRoutes: Router = router;
