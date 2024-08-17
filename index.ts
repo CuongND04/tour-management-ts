1133ce1fd7cf0e8ac675ae90280448c0d18e6c68
Nguyễn Đức Cường <uteu237@gmail.com>
2024-08-17 18:08:01 +0700

    ✨ get tour list

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import sequelize from "./config/dataase";
 import dotenv from "dotenv";
+import Tour from "./models/tour.model";
 dotenv.config();
 sequelize; // nó là một cái biến nên chỉ cần gọi ra thôi
 
@@ -11,8 +12,11 @@ const port: string | number = process.env.PORT || 3000;
 app.set("views", `./views`);
 app.set("view engine", "pug");
 
-app.get("/tours", (req: Request, res: Response) => {
-  res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({ raw: true });
+  res.render("client/pages/tours/index", {
+    tours: tours,
+  });
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
