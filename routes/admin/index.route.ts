e34db3171f5de2c66111ffb6582be170fabea3d1
Nguyễn Đức Cường <uteu237@gmail.com>
2024-08-19 22:44:23 +0700

    ✨ tour list (admin side)

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,11 +1,13 @@
 import { Express } from "express";
 import { categoryRoutes } from "./category.route";
-import { systemConfig } from "../../config/system";
+import { tourRoutes } from "./tour.route";
 
+import { systemConfig } from "../../config/system";
 const adminRoutes = (app: Express): void => {
   const path = `/${systemConfig.prefixAdmin}`;
 
   app.use(`${path}/categories`, categoryRoutes);
+  app.use(`${path}/tours`, tourRoutes);
 };
 
 export default adminRoutes;
